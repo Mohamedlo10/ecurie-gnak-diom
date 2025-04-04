ce39f06571ee7ad07908145f521e942f46459054
papesocendiaye <pape.soce.ndiaye@outlook.com>
2025-04-04 21:47:28 +0000

    plagiat nice

--- backend/src/models/plagiatCopieModel.js
+++ backend/src/models/plagiatCopieModel.js
@@ -1,39 +1,13 @@
-import supabase from "../config/db.js";
+import sql from "../config/db.js";
 
-const PlagiatCopieModel = {
-    async createPlagiatCopie({ idplagiat, idcopie, pourcentageplagiat }) {
-        const { data, error } = await supabase
-            .from("plagiatcopie")
-            .insert([{ idplagiat, idcopie, pourcentageplagiat }]);
-
-        if (error) throw error;
-        return data;
-    },
-
-    async getAllPlagiatCopies() {
-        const { data, error } = await supabase.from("plagiatcopie").select("*");
-        if (error) throw error;
-        return data;
-    },
-
-    async getPlagiatCopieById(id) {
-        const { data, error } = await supabase
-            .from("plagiatcopie")
-            .select("*")
-            .eq("id", id)
-            .single();
-        if (error) throw error;
-        return data;
-    },
-
-    async deletePlagiatCopie(id) {
-        const { data, error } = await supabase
-            .from("plagiatcopie")
-            .delete()
-            .eq("id", id);
-        if (error) throw error;
-        return data;
-    }
-};
-
-export default PlagiatCopieModel;
+export const createPlagiatCopie = async (idplagiat, idutilisateur, idsujet, idcopie, similarity) => {
+    
+    // Requête SQL pour insérer dans la table plagiatcopie
+    const query = await sql`
+      INSERT INTO plagiatcopie (idplagiat, idutilisateur, idsujet, idcopie, pourcentage)
+      VALUES (${idplagiat}, ${idutilisateur}, ${idsujet}, ${idcopie}, ${similarity})
+      RETURNING *;
+    `;
+    // On renvoie la première ligne insérée
+    return query[0];
+  };
