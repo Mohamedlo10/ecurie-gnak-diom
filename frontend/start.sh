75c316373b54349513fe4e3b803c99a63c4505a6
papesocendiaye <pape.soce.ndiaye@outlook.com>
2025-04-07 01:55:16 +0000

    version

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/elite/ecurie-gnak-diom/frontend
-export PATH=/var/www/elite/.nvm/versions/node/v18.20.7/bin:/usr/bin:/bin
+export PATH=/var/www/elite/.nvm/versions/node/v20.19.0/bin:/usr/bin:/bin
 export NODE_ENV=production
 export PORT=3006
 
